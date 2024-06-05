a61173bcd999de2b09a9a4f687a309c8c5e41cea
ChienChihYeh <miroyeh1986@gmail.com>
2024-06-05 17:57:25 +0800

    modified style

--- src/components/DialogInput.vue
+++ src/components/DialogInput.vue
@@ -64,7 +64,7 @@ const sendMessage = () => {
   bottom: 0;
   height: 7rem;
   padding: 2rem;
-  background: linear-gradient(transparent 0%, #fff 15%, #fff 100%);
+  background: linear-gradient(transparent 0%, #fff 5%, #fff 100%);
 }
 
 .dialog-input-container {
